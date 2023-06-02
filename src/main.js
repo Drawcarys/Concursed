233cc6e95766d66863635e4f6a6247b44173e1f1
Drawcarys <briz.martinez.19201@itsmante.edu.mx>
2023-06-02 12:40:39 -0600

    Resuelve error que pudohaber costado el proyecto y la estabilidad emocional de la jefa de equipo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import PantallaAdministrador from './components/PantallaAdministrador.vue'
+
+import DatosConcurso from './components/Administrador/DatosConcurso.vue';
+import BotonesAdministrador from './components/Administrador/BotonesAdministrador.vue';
+
+createApp(BotonesAdministrador).mount('#botonesAdministrador');
+createApp(DatosConcurso).mount('#datosConcurso');
+createApp(PantallaAdministrador).mount('#pantallaAdministrador')
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+import { createApp } from 'vue'
+
+import RegistroProyecto from './components/RegistroProyecto.vue'
 
 
-
+createApp(RegistroProyecto).mount('#registroProyecto')
