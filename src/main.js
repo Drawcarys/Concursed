2f9012640bc4670b4f00bff80e98e6af582c6032
Drawcarys <briz.martinez.19201@itsmante.edu.mx>
2023-06-04 11:37:11 -0600

    Arregla problemas

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ createApp(BotonesAdministrador).mount('#botonesAdministrador');
 createApp(DatosConcurso).mount('#datosConcurso');
 createApp(PantallaAdministrador).mount('#pantallaAdministrador')
 import App from './App.vue'
-
+import PantallaPrincipal from './PantallaPrincipal.vue'
+import JuezPantalla_principal from './JuezPantalla_principal.vue'
 createApp(App).mount('#app')
-
+createApp(PantallaPrincipal).mount('#pantallaPrincipal')
+createApp(JuezPantalla_principal).mount('#juezPantallaPrincipal')
 import RegistroProyecto from './components/RegistroProyecto.vue'
 
 
