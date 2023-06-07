ed5d43f4f09123daca57b0c2418a3bb9207d94b4
Oziel03 <hernandez.vazquez.19108@itsmante.edu.mx>
2023-06-07 11:55:23 -0600

    Basics querys and mutations, register and insert

--- src/graphql.js
+++ src/graphql.js
@@ -7,3 +7,60 @@ export const ALL_ADMINS = gql`query admins {
       correo
     }
   }`
+
+  export const ALL_AREAS = gql`query areas {
+    area {
+      nombreArea
+    }
+  }`
+
+  export const ALL_CATEGORIAS = gql`query categorias {
+    categoria {
+      nombreCategoria
+    }
+  }`
+
+  export const ALL_JUECES = gql`query jueces {
+    juez {
+      persona {
+        nombre
+        apellido1
+        apellido2
+        institucion
+      }
+      juez_sede_cates {
+        sede {
+          nombreSede
+        }
+        categorium {
+          nombreCategoria
+        }
+      }
+    }
+  }`
+
+  export const ALL_PARTICIPANTES = gql`query participantes {
+    participante {
+      persona {
+        nombre
+        apellido1
+        apellido2
+      }
+      proy_parts {
+        proyecto {
+          nombreProyecto
+        }
+      }
+    }
+  }`
+
+  export const ALL_PROYECTOS = gql`query proyectos {
+    proyecto {
+      nombreProyecto
+      areaProyecto
+      categoriaProyecto
+      folio
+      modalidad
+      sedeProyecto
+    }
+  }`
